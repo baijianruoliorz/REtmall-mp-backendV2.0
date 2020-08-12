355adde10822c47ac22a3f06b2ab8f1bab2f02fe
liqiqiorz <1099462011@qq.com>
2020-08-12 09:34:31 +0800

    EasterEgg

---
+++ src/main/java/com/yxr/tmall/controller/EasterEggController.java
@@ -0,0 +1,22 @@
+package com.yxr.tmall.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liqiqi_tql
+ * @date 2020/8/12 -9:30
+ */
+@RestController
+public class EasterEggController {
+
+    @GetMapping("/")
+    public String EasterEgg(){
+        return "Thank you for visit my project," +
+                "but this one isn't have a frontend now" +
+                "you can visit my github and pull it to develop by yourself" +
+                "yours sincerely:" +
+                "liqiqiorz" +
+                "twitter:@roz97101";
+    }
+}
